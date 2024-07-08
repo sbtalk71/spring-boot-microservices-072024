1ef4ce8d0c386bad66bcd7a79a79f156ca26b4bc
sbtalk71 <sbtalk@yahoo.com>
2024-07-08 11:11:21 +0530

    no DI Example added

---
+++ NoSpringDI/src/com/demo/Message.java
@@ -0,0 +1,23 @@
+package com.demo;
+
+public class Message {
+
+	private String greetings;
+
+	public Message() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Message(String greetings) {
+		this.greetings = greetings;
+	}
+
+	public String getGreetings() {
+		return greetings;
+	}
+
+	public void setGreetings(String greetings) {
+		this.greetings = greetings;
+	}
+
+}
