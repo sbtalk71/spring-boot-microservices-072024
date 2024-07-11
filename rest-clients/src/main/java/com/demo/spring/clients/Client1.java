9fc138f56e3865e1c5ede7a344f5c245d6493e4f
sbtalk71 <sbtalk@yahoo.com>
2024-07-11 13:21:33 +0530

    projects and docs updated

--- rest-clients/src/main/java/com/demo/spring/clients/Client1.java
+++ rest-clients/src/main/java/com/demo/spring/clients/Client1.java
@@ -1,68 +1,21 @@
 package com.demo.spring.clients;
 
-import java.util.List;
-
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 public class Client1 {
 
 	public static void main(String[] args) {
-		String base_url = "http://localhost:8080/emp";
+		String base_url = "http://localhost:8080/user";
 
 		RestTemplate rt = new RestTemplate();
 
-		ResponseEntity<String> respEnt = rt.getForEntity(base_url + "/104", String.class);
+		ResponseEntity<String> respEnt = rt.getForEntity(base_url, String.class);
 
 		System.out.println(respEnt.getBody());
 
 		
 		
-		
-		// Spring 6.1
-		RestClient rc = RestClient.create(base_url);
-		String resp = rc.get().uri("/106").accept(MediaType.APPLICATION_JSON).retrieve().body(String.class);
-
-		System.out.println(resp);
-
-		// *****
-		Emp e = rc.get().uri("/107").accept(MediaType.APPLICATION_JSON).retrieve().body(Emp.class);
-
-		System.out.println(e);
-
-		// ***
-
-		List<Emp> empList = rc.get().accept(MediaType.APPLICATION_JSON).retrieve()
-				.body(new ParameterizedTypeReference<List<Emp>>() {
-				});
-
-		for (Emp emp : empList) {
-			System.out.println(emp);
-		}
-	/*	
-	String postResponse =	rc.post()
-							.body(new Emp(125, "Kiran", "Bhopal", 46000))
-							.contentType(MediaType.APPLICATION_JSON)
-							.accept(MediaType.APPLICATION_JSON)
-							.retrieve()
-							.body(String.class);
-	
-	System.out.println(postResponse);
-	*/
-	
-	
-	String delResponse =	rc.delete()
-							.uri("/124")
-							.accept(MediaType.TEXT_PLAIN)
-							.retrieve()
-							.body(String.class);
-
-System.out.println(delResponse);
-	
-
 	}
 
 }
--- spring6-web-security/src/main/java/com/demo/spring/SecurityConfig.java
+++ spring6-web-security/src/main/java/com/demo/spring/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,19 +23,13 @@ public class SecurityConfig {
 
 	@Autowired
 	DataSource dataSource;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests(
-				httpsec -> httpsec
-						.requestMatchers("/user")
-						.hasAnyRole("USER","ADMIN")
-						.requestMatchers("/admin")
-						.hasRole("ADMIN")
-						.requestMatchers("/info")
-						.permitAll()
-						.anyRequest()
-						.authenticated())
+				httpsec -> httpsec.requestMatchers("/user").hasAnyRole("USER", "ADMIN").requestMatchers("/admin")
+						.hasRole("ADMIN").requestMatchers("/info").permitAll().anyRequest().authenticated())
 				.httpBasic(Customizer.withDefaults());
 
 		return http.build();
@@ -52,44 +47,54 @@ public class SecurityConfig {
 	 * .disabled(true); }
 	 */
 	/*
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails pavan = User					
-							.withUsername("pavan")
-							.password("$2a$10$td6KTADr4d4jB1/HxNzFxu3dRGaIANb6KFXOClywO7nUvzLmaHMJG")
-							.roles("USER")
-							.build();
-		UserDetails shantanu = User					
-				.withUsername("shantanu")
-				.password("$2a$10$td6KTADr4d4jB1/HxNzFxu3dRGaIANb6KFXOClywO7nUvzLmaHMJG")
-				.roles("ADMIN")
-				.build();
-		
-		UserDetails arun = User					
-				.withUsername("arun")
-				.password("$2a$10$td6KTADr4d4jB1/HxNzFxu3dRGaIANb6KFXOClywO7nUvzLmaHMJG")
-				.roles("USER")
-				.disabled(true)
-				.build();
-		
-		return new InMemoryUserDetailsManager(pavan,shantanu,arun);
-	}
-*/
+	 * @Bean public UserDetailsService userDetailsService() { UserDetails pavan =
+	 * User .withUsername("pavan")
+	 * .password("$2a$10$td6KTADr4d4jB1/HxNzFxu3dRGaIANb6KFXOClywO7nUvzLmaHMJG")
+	 * .roles("USER") .build(); UserDetails shantanu = User
+	 * .withUsername("shantanu")
+	 * .password("$2a$10$td6KTADr4d4jB1/HxNzFxu3dRGaIANb6KFXOClywO7nUvzLmaHMJG")
+	 * .roles("ADMIN") .build();
+	 * 
+	 * UserDetails arun = User .withUsername("arun")
+	 * .password("$2a$10$td6KTADr4d4jB1/HxNzFxu3dRGaIANb6KFXOClywO7nUvzLmaHMJG")
+	 * .roles("USER") .disabled(true) .build();
+	 * 
+	 * return new InMemoryUserDetailsManager(pavan,shantanu,arun); }
+	 */
+	/*
+	 * @Autowired public void configureDbStore(AuthenticationManagerBuilder auth)
+	 * throws Exception{ auth.jdbcAuthentication() .dataSource(dataSource) //queries
+	 * are optional if using default schema
+	 * .usersByUsernameQuery("select username,password,enabled from users where username=?"
+	 * )
+	 * .authoritiesByUsernameQuery("select username, authority from authorities where username=?"
+	 * ); }
+	 * 
+	 */
 	@Autowired
-	public void configureDbStore(AuthenticationManagerBuilder auth) throws Exception{
-		auth.jdbcAuthentication()
-			.dataSource(dataSource)
-			//queries are optional if using default schema
-			.usersByUsernameQuery("select username,password,enabled from users where username=?")
-			.authoritiesByUsernameQuery("select username, authority from authorities where username=?");
+	public void configureLdapStore(AuthenticationManagerBuilder auth) throws Exception {
+		auth.ldapAuthentication()
+				.contextSource()
+				.url("ldap://localhost:10389/dc=jboss,dc=com")
+				.managerDn("uid=admin,ou=system")
+				.managerPassword("secret")
+				.and()
+				.userSearchBase("ou=users")
+				.userSearchFilter("uid={0}")
+				.groupSearchBase("ou=roles")
+				.groupSearchFilter("uniqueMember={0}")
+				.groupRoleAttribute("cn")
+				.passwordCompare()
+				.passwordEncoder(new LdapShaPasswordEncoder())
+				.passwordAttribute("userPassword");
+		System.out.println("LDAP initialized...");
+
 	}
-	
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 
 	}
 
-	
 }
