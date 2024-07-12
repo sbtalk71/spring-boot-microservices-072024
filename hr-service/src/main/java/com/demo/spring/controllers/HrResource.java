20cb8286638cec2bcbc21f8983ec116eee82b429
sbtalk71 <sbtalk@yahoo.com>
2024-07-12 11:25:07 +0530

    hr-services added

---
+++ hr-service/src/main/java/com/demo/spring/controllers/HrResource.java
@@ -0,0 +1,23 @@
+package com.demo.spring.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/hr")
+public class HrResource {
+
+	@Autowired
+	RestTemplate rt;
+
+	@GetMapping(path = "/emp/{empid}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> getEmDetails(@PathVariable("empid") String empId) {
+		return rt.getForEntity("http://localhost:8080/emp/" + empId, String.class);
+	}
+}
--- spring6-web-security-jwt/src/main/java/com/demo/spring/SecurityConfig.java
+++ spring6-web-security-jwt/src/main/java/com/demo/spring/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.csrf().disable()
+		http.csrf(csrf->csrf.disable())
 				.authorizeHttpRequests(httpsec -> httpsec.requestMatchers("/user").hasAnyRole("USER", "ADMIN")
 						.requestMatchers("/admin").hasRole("ADMIN").requestMatchers("/authenticate").permitAll()
 						.anyRequest().authenticated())
